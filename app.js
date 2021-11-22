c2acd42b2222fe7a2cf1e2fc3ee153e2fb41b7f1
Seung Hyun Son <james@gracefulrain.co>
2021-11-22 11:45:20 +0900

    1

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+const PORT = process.argv[2]
+
+app.get('/', (req, res) => {
+  console.log('GET Request')
+  res.send(`Served from ${process.pid}`)
+})
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
+})
